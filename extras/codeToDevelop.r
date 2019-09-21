ed764aa2f871672b76ee185cff5824925356f7ed
chandryou <applegna@gmail.com>
2019-09-21 13:18:52 -0400

    modify code to develop code

--- extras/codeToDevelop.r
+++ extras/codeToDevelop.r
@@ -1,4 +1,42 @@
-# package.skeleton(name = "Prometheus", path ="C:/Users/apple/git/abmi",
-#                  force = TRUE)
+# Copyright 2019 Observational Health Data Sciences and Informatics
+#
+# This file is part of Prometheus
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-roxygen2::roxygenise()
+# Format and check code ---------------------------------------------------
+OhdsiRTools::formatRFolder()
+OhdsiRTools::checkUsagePackage("Prometheus")
+OhdsiRTools::updateCopyrightYearFolder()
+
+# Create manual -----------------------------------------------------------
+shell("rm extras/Prometheus.pdf")
+shell("R CMD Rd2pdf ./ --output=extras/Prometheus.pdf")
+
+# Create vignettes ---------------------------------------------------------
+# rmarkdown::render("vignettes/UsingPrometheus.Rmd",
+#                   output_file = "../inst/doc/UsingSkeletonPackage.pdf",
+#                   rmarkdown::pdf_document(latex_engine = "pdflatex",
+#                                           toc = TRUE,
+#                                           number_sections = TRUE))
+
+# Store environment in which the study was executed -----------------------
+OhdsiRTools::insertEnvironmentSnapshotInPackage("Prometheus")
+
+# Format and check code ---------------------------------------------------
+OhdsiRTools::formatRFolder()
+OhdsiRTools::checkUsagePackage("Prometheus")
+OhdsiRTools::updateCopyrightYearFolder()
+
+#insert Environment
+OhdsiRTools::insertEnvironmentSnapshotInPackage("Prometheus")
